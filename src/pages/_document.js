1fe0b3b1cfc84ffc2a4bde9fef550bf412eb3e1c
“gabriel6181997” <gabriel6181997@gmail.com>
2021-02-08 11:53:30 +0800

    Update Document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,3 @@
-import type { DocumentContext, DocumentInitialProps } from "next/document";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
@@ -10,7 +9,6 @@ export default class MyDocument extends Document {
     return(
       <Html lang="ja">
         <Head>
-          <link rel="sobolon" sizes="48x48" href={favicon.ico}></link>
         </Head>
         <body>
           <Main />
