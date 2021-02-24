cf8fff78b4d20fbded1d422bdd1117c319e425f2
“gabriel6181997” <gabriel6181997@gmail.com>
2021-02-24 14:03:28 +0800

    Add AppProps in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import 'src/styles/globals.scss';
 import '@fortawesome/fontawesome-free/css/all.css';
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps):JSX.Element {
   return <Component {...pageProps} />
 }
 
